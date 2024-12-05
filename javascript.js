4b639ca8dffba80b64e955144a2f471b64dda048
HaileabWolde <womahaileab1428@gmail.com>
2024-12-05 11:31:44 +0300

    Added Logic for it to give the player to choose to enchance user interactivty

--- javascript.js
+++ javascript.js
@@ -1,75 +1,79 @@
+let humandigit = 0;
+let computerdigit = 0;
+let smallDiv = document.querySelector(".text-DIV")
 function getAIChoice() {
   let game_ChoiceArray = [
-      "./paper2.png",
-      "./rock.png",
-      "./scissor.png"
+    "./paper2.png",
+    "./rock.png",
+    "./scissor.png"
   ];
-  let index = Math.floor(Math.random() * game_ChoiceArray.length); // Fixed Math.random usage
+  let index = Math.floor(Math.random() * game_ChoiceArray.length);
   let choice = game_ChoiceArray[index];
 
   const pic = document.createElement('img');
   pic.src = choice;
-  
-  // Get the first element with the class "computer"
-  let computerImage = document.querySelector(".computer"); // Use querySelector for a single element
-  // Clear any previous AI choice images (optional)
-  computerImage.innerHTML = ""; 
 
-  computerImage.appendChild(pic); // Set the `src` attribute for the selected element
+  let computerImage = document.querySelector(".computer");
+  computerImage.innerHTML = ""; // Clear previous AI choice
+  computerImage.appendChild(pic); // Add new AI choice
   return choice;
 }
-  function playGame(e){
-    function playRound(humanChoice, aiChoice) {
-      if(humanChoice == "rock" && aiChoice == "./scissor.png"){
-           console.log("You Win! Rock Beats Scissor");
-           humandigit = humandigit + 1;
-      }
-      else if(humanChoice == "rock" && aiChoice == "./paper2.png") {
-         console.log("You Lose! PAPER Beats Rock");
-         computerdigit = computerdigit + 1;
-      }
-      else if(humanChoice == "rock" && aiChoice == "./rock.png") {
-         console.log("IT IS A TIE!")
-      }
-       else if(humanChoice == "paper2" && aiChoice == "./rock.png") {
-         console.log("You Win! Paper Beats Rock");
-         humandigit = humandigit + 1;
-      }
-       else if(humanChoice == "paper2" && aiChoice == "./scissor.png") {
-         console.log("You Win! Paper Beats Scissor");
-         humandigit = humandigit + 1;
-      }
-      else if(humanChoice == "paper2" && aiChoice == "./paper2.png") {
-         console.log("IT IS A TIE!");
-      }
-       else if(humanChoice == "scissor" && aiChoice == "./paper2.png") {
-         console.log("You Win! Scissor Beats Paper");
-         humandigit = humandigit + 1;
-      }
-       else if(humanChoice == "scissor" && aiChoice == "./rock.png") {
-         console.log("You Lose! Rock Beats Scissor");
-         computerdigit = computerdigit + 1;
-      }
-      else {
-        console.log("It is A TIE!")
-      }
-   }
-   let humandigit = 0;
-   let computerdigit = 0;
-   let sum = 0;
-   for (let i = 0; sum < 5; i++){
-    let humanChoice = e.target.id;
-    let aiChoice = getAIChoice();
-      playRound(humanChoice, aiChoice);
-      sum = humandigit + computerdigit;
-      console.log(humandigit);
-      console.log(computerdigit);
-   }
 
+function playRound(humanChoice, aiChoice) {
+   smallDiv.innerHTML= "";
+  if (humanChoice == "rock" && aiChoice == "./scissor.png") {
+    smallDiv.textContent = "You Win! Rock Beats Scissor";
+    humandigit += 1;
+  } else if (humanChoice == "rock" && aiChoice == "./paper2.png") {
+    smallDiv.textContent = "You Lose! PAPER Beats Rock";
+    computerdigit += 1;
+  } else if (humanChoice == "rock" && aiChoice == "./rock.png") {
+    smallDiv.textContent = "IT IS A TIE!";
+  } else if (humanChoice == "paper" && aiChoice == "./rock.png") {
+    smallDiv.textContent = "You Win! Paper Beats Rock";
+    humandigit += 1;
+  } else if (humanChoice == "paper" && aiChoice == "./scissor.png") {
+    smallDiv.textContent = "You Lose! Scissor Beats Paper";
+    computerdigit += 1;
+  } else if (humanChoice == "paper" && aiChoice == "./paper2.png") {
+    smallDiv.textContent = "IT IS A TIE!";
+  } else if (humanChoice == "scissor" && aiChoice == "./paper2.png") {
+    smallDiv.textContent = "You Win! Scissor Beats Paper";
+    humandigit += 1;
+  } else if (humanChoice == "scissor" && aiChoice == "./rock.png") {
+    smallDiv.textContent = "You Lose! Rock Beats Scissor";
+    computerdigit += 1;
+  } else {
+    smallDiv.textContent = "IT IS A TIE!";
   }
-   let player = document.querySelector(".player");
-   player.addEventListener('click', playGame);
+}
 
+function playGame(e) {
+  let humanChoice = e.target.id;
+  let aiChoice = getAIChoice();
+  console.log(humanChoice);
+  console.log(aiChoice);
+  playRound(humanChoice, aiChoice);
+  scoreBoard.textContent = `${humandigit}`
+  playerBoard.textContent = `${computerdigit}`
+ //scoreBoard.textContent = `Player: ${humandigit}, Computer: ${computerdigit}`;
+
+  if (humandigit + computerdigit >= 5) {
+    let winner =
+      humandigit > computerdigit
+        ? "You Win!"
+        : humandigit < computerdigit
+        ? "Computer Wins!"
+        : "It's a Tie!";
+
+    // Reset scores for a new game
+    humandigit = 0;
+    computerdigit = 0;
+  }
+}
+
+let player = document.querySelector(".player");
+player.addEventListener("click", playGame);
   
 /*
 function getComputerChoice(){
